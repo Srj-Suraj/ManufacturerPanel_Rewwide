69f13401fa0b09df64c66a11061de6b5ea153982
Srj-Suraj <suraj.kumar@eleganteventures.com>
2022-01-14 23:33:00 +0530

    12

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Login/Login';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h4>SRJ</h4>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //     <h4>SRJ</h4>
+    //   </header>
+    // </div>
+
+    <Router>
+    <Routes>
+        <Route path="/" element={<> <Login/> </>} /> 
+              
+    </Routes>
+</Router>
   );
 }
 
