43f7cb4dac97e82811e7eb0d477f8fd63a7f8d40
Srj-Suraj <suraj.kumar@eleganteventures.com>
2022-01-15 00:34:49 +0530

    14

--- src/App.js
+++ src/App.js
@@ -6,28 +6,28 @@ import Login from './Login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h4>SRJ</h4>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //     <h4>SRJ</h4>
+    //   </header>
+    // </div>
 
 //     <Router>
 //     <Routes>
 //         <Route path="/" element={<> <Login/> </>} /> 
-              
+    <Login/>
 //     </Routes>
 // </Router>
   );
