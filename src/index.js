d7c4fbddaacbec31329118c56b79c0c87c5fe382
Srj-Suraj <suraj.kumar@eleganteventures.com>
2022-01-15 21:55:43 +0530

    retun to affiliate project

--- src/index.js
+++ src/index.js
@@ -1,34 +1,48 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-// import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-import  Navigation from "./Pages/Navigation";
-import  Footer from "./Pages/Footer";
-import  Home from "./Pages/Home";
-import  About from "./Pages/About";
-import  Contact from "./Pages/Contact";
-// import  Blog from "./Pages/Blog";
-// import  Posts from "./Pages/Posts";
-// import  Post from "./Pages/Post";
+// css
+import './index.css';
+
+// components
+import Login from './Login/Login';
+import Register from './Register/Register';
+import SideBar from './Components/SideBar';
+import NavBar from './Components/NavBar';
+// pages
+import AllCategory from './AllCategory/AllCategory';
+import Products from './Products/Products';
+import Transactions from './Transactions/Transactions';
+import ProfileSetting from './ProfileSetting/ProfileSetting';
+import Dashboard from './Dashboard/Dashboard';
+import Revenue from './Revenue/Revenue';
 
 ReactDOM.render(
   <Router basename="/">
-    <Navigation />
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/contact" element={<Contact />} />
-      {/* <Route path="/blog" element={<Blog />}>
-        <Route path="" element={<Posts />} />
-        <Route path=":postSlug" element={<Post />} />
-      </Route> */}
-    </Routes>
-    <Footer />
-  </Router>,
+                      {/* <Route path="/" element={<Home />} />
+                      <Route path="/about" element={<About />} />
+                      <Route path="/contact" element={<Contact />} /> */}
 
+      
+          <Route path="/" element={<> <Login/> </>} /> 
+          <Route path="/login" element={<> <Login/>  </>}   /> 
+          <Route path="/register" element={<>  <Register/>  </>}   /> 
+
+          <Route path="/all-category" element={<>  <SideBar/> <NavBar/> <AllCategory/> </>}   /> 
+          <Route path="/products" element={<> <SideBar/> <NavBar/> <Products/> </>}   /> 
+          <Route path="/transactions" element={<> <SideBar/> <NavBar/> <Transactions/> </>}   /> 
+          <Route path="/profile-setting" element={<> <SideBar/> <NavBar/> <ProfileSetting/> </>}   /> 
+          <Route path="/dashboard" element={<> <SideBar/> <NavBar/> <Dashboard/> </>}   /> 
+          <Route path="/revenue" element={<> <SideBar/> <NavBar/> <Revenue/> </>}   /> 
+                  
+    </Routes>
+    
+  </Router>
+  
+  
+  ,
   document.getElementById("root")
 );
-
-// serviceWorker.unregister();
