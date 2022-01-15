56038fbcc0ec149c4b10aa258ac505bf91ab1fd4
Srj-Suraj <suraj.kumar@eleganteventures.com>
2022-01-15 20:50:35 +0530

    new files

--- src/index.js
+++ src/index.js
@@ -1,20 +1,34 @@
-import React,{ useState } from "react";
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import 'permissions-policy';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+// import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-
+import  Navigation from "./Pages/Navigation";
+import  Footer from "./Pages/Footer";
+import  Home from "./Pages/Home";
+import  About from "./Pages/About";
+import  Contact from "./Pages/Contact";
+// import  Blog from "./Pages/Blog";
+// import  Posts from "./Pages/Posts";
+// import  Post from "./Pages/Post";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router basename="/">
+    <Navigation />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      {/* <Route path="/blog" element={<Blog />}>
+        <Route path="" element={<Posts />} />
+        <Route path=":postSlug" element={<Post />} />
+      </Route> */}
+    </Routes>
+    <Footer />
+  </Router>,
+
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// serviceWorker.unregister();
